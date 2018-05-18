e0bfa32e723dfec49defdaa4dcb4b6c9532f2d46
Anny Caroline <sousa.annyc@gmail.com>
2018-05-18 00:58:49 -0300

    Imagem na Tela principal

--- app/src/main/java/com/wordpress/diariogamedev/tcp4_2018_1/MainActivity.java
+++ app/src/main/java/com/wordpress/diariogamedev/tcp4_2018_1/MainActivity.java
@@ -1,13 +1,17 @@
 package com.wordpress.diariogamedev.tcp4_2018_1;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    DrawView drawView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        drawView = new DrawView(this);
+        setContentView(drawView);
     }
 }
