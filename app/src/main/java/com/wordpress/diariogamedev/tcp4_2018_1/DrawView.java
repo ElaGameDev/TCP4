0cb40153a54178ad19fd22896c371b76f5ec6c76
Anny Caroline <sousa.annyc@gmail.com>
2018-05-18 01:05:41 -0300

    Imagem na tela principal

---
+++ app/src/main/java/com/wordpress/diariogamedev/tcp4_2018_1/DrawView.java
@@ -0,0 +1,29 @@
+package com.wordpress.diariogamedev.tcp4_2018_1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class DrawView extends View{
+    int x, y;
+    Bitmap bmp;
+
+    public DrawView (Context context){
+        super(context);
+
+        x = 100;
+        y = 100;
+
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.pomboteste);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        //Paint p = new Paint();
+        canvas.drawBitmap(bmp, 10, 10, null);
+    }
+}
